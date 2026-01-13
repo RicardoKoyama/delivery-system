f9789e612fa0345d2d3d7afed59c69ed2bef66dc
RicardoKoyama <koyamaricardo@gmail.com>
2026-01-13 20:29:43 -0300

    Ajustes iniciais

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const env = {
   NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: Number(process.env.PORT || 3000),
+  PORT: Number(process.env.PORT),
 
   DATABASE_URL: process.env.DATABASE_URL,
 
